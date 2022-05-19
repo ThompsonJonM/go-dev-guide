7c2fa991d39746758c325918f3bd7e5910b7ce8c
ThompsonJonM <thompson.jonm@gmail.com>
2022-05-19 14:46:57 -0400

    Add sorting dir

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,15 @@
 package main
+
+import "testing"
+
+func TestCreateNewDeck(t *testing.T) {
+	deck := createNewDeck()
+
+	if len(deck) != 52 {
+		t.Errorf("Expected deck to be size %d, got %d", 52, len(deck))
+	}
+
+	if deck[0] != "Ace of Spades" {
+		t.Errorf("Expected %s, got %s", "Ace of Spades", deck[0])
+	}
+}
